dd8fbde4b844274589a5a5490be094d1818ddc3c
Rodrigo Blefari Goncalves <rodrigo.blefari@gmail.com>
2020-02-10 23:08:52 -0300

    Alteracoes basicas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,32 @@
 import React, { useState } from 'react'
 import './Login.css'
 
+import api from '../services/api'
+
 import Logo from '../assets/logo.svg';
 
 
-export default function Login() {
-    const [username, setUsername] = useState('')
+export default function Login({ history }) {
+    const [username, setUsername] = useState('marcos')
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
 
-        console.log(username)
+        const response = await api.post('/devs', {
+            username
+        })
+
+        const { _id } = response.data
+
+        history.push(`/dev/${_id}`)
     }
     return (
 
         <div className="login-container">
             <h1>{username}</h1>
             <form
-                onSubmit= {handleSubmit}
+                onSubmit={handleSubmit}
             >
-
                 <img src={Logo} className="App-logo" alt="logo" />
                 <input
                     value={username}
